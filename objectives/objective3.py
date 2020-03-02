2cf083af61d8ab097317b144092a0a38fd08764e
purbashacg9 <purbasha@paradata.io>
2020-03-02 10:45:16 -0800

    Script for objective 3 -> combine log files into one file and extract relevant columns

---
+++ objectives/objective3.py
@@ -0,0 +1,60 @@
+import argparse
+import pandas as pd 
+import os 
+
+def combine_files(local_dir: str, file_format: str, output_file: str) -> None: 
+    """
+    Reads all files (recursively) in 'local_dir' with format matching 'file_format', combines the files into a CSV.   
+    Name of the CSV file is taken from output_file argument. 
+    local_dir:: str - Directory containing the files to be combined. This is a relative path from the directory where the script is running. 
+    file_format:: str - Only files of this type are combined for processing
+
+    return:: None
+    """
+    if not os.path.isdir(os.path.join('.',local_dir)): 
+        print(f'No matching directory for {local_dir}. Please provide correct directory name and try again.')
+        return 
+    
+    dfs = [] 
+    for path, _, files in os.walk(local_dir): 
+        for filename in files:
+            if filename.endswith(file_format): 
+                df = pd.read_json(
+                    os.path.join(path,filename), 
+                    orient='records', 
+                    compression='gzip'
+                )
+                dfs.append(df)
+            else:
+                print(f'Skipping file {filename} as it is not in json.gz format') 
+
+    #print(f'Number of files combined {len(dfs)}')
+    if len(dfs):
+        data_for_processing = pd.concat(dfs, ignore_index=True)
+        data = [] 
+        for _, row in data_for_processing.iterrows():
+            desired_data = {}    
+            desired_data['eventTime'] = row['Records'].get('eventTime')
+            desired_data['sourceIPAddress'] = row['Records'].get('sourceIPAddress')
+            desired_data['userIdentityArn'] = row['Records'].get('userIdentity').get('arn')
+            desired_data['userIdentityAccountId'] = row['Records'].get('userIdentity').get('accountId')
+            desired_data['userIdentityType'] = row['Records'].get('userIdentity').get('type')
+            desired_data['eventName'] = row['Records'].get('eventName')
+            data.append(desired_data)
+
+        filtered_data = pd.DataFrame(data=data)
+        filtered_data.to_csv(output_file)
+        
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Combine CloudTrail logs and generate a CSV for further analysis')
+    parser.add_argument("-d", "--local_dir", required=True, help="Local directory that contains the files downloaded from S3 bucket")
+    parser.add_argument("-f", "--file_format", required=True, help="Format of files to combine")
+    parser.add_argument("-o", "--output_file", required=True, help="Name of output file")
+
+    args = vars(parser.parse_args())
+    local_dir = args.get('local_dir')
+    file_format = args.get('file_format')
+    output_file = args.get('output_file')
+    
+    combine_files(local_dir, file_format, output_file)
